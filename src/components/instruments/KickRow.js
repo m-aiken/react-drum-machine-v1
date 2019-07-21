f6d52fd6fe2a19cf38704bb9b22796e0303949dd
Matt Aiken <mattaiken@hotmail.co.uk>
2019-07-21 15:04:50 +0100

    restyled responsive

--- src/components/instruments/KickRow.js
+++ src/components/instruments/KickRow.js
@@ -90,19 +90,21 @@ class KickRow extends Component {
                 steps={this.state.kickSteps}
                 updateChecked={this.updateChecked.bind(this, dispatch)}
               />
-              <PitchSlider
-                value={this.state.pitchSliderValue}
-                changePitch={this.changePitch.bind(this, dispatch)}
-                note={this.state.note}
-              />
-              <ReverbSlider
-                value={this.state.reverb}
-                changeReverb={this.changeReverb}
-              />
-              <VolumeSlider
-                value={this.state.volume}
-                changeVolume={this.changeVolume}
-              />
+              <div className="slider-container">
+                <PitchSlider
+                  value={this.state.pitchSliderValue}
+                  changePitch={this.changePitch.bind(this, dispatch)}
+                  note={this.state.note}
+                />
+                <ReverbSlider
+                  value={this.state.reverb}
+                  changeReverb={this.changeReverb}
+                />
+                <VolumeSlider
+                  value={this.state.volume}
+                  changeVolume={this.changeVolume}
+                />
+              </div>
             </div>
           );
         }}
