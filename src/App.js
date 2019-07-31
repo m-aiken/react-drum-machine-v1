30ee42af8ddd4fba5296b3e604007bade9bf842a
Matt Aiken <mattaiken@hotmail.co.uk>
2019-07-31 21:24:23 +0100

    Audio buffer issue resolved

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tone from 'tone';
 
 import StartModal from './components/StartModal';
 import Playback from './components/Playback';
+import { checkBuffers } from './components/ToneComponents';
 
 import './App.css';
 import './css/headerTransport.css';
@@ -15,14 +16,21 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      soundsLoaded: false,
       modal: true
     };
   }
 
+  // Create audio context
+  // Check that all 10 audio buffers have loaded, promise resolved when true
   componentDidMount() {
     audioCtx = new Tone.Context();
+    checkBuffers().then(() => {
+      this.setState({ soundsLoaded: true });
+    });
   }
 
+  // Resume audio context on user interaction as per WebAudioAPI requirements
   closeModal = () => {
     audioCtx.resume();
     this.setState({ modal: false });
@@ -33,6 +41,7 @@ class App extends Component {
       <Provider>
         <div className="project-container">
           <StartModal
+            soundsLoaded={this.state.soundsLoaded}
             modalStatus={this.state.modal}
             closeModal={this.closeModal}
           />
